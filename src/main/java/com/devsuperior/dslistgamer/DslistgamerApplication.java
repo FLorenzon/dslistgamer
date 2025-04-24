4115a303dc03dae269dc4b6afc880c8e257f6979
Felipe Lorenzon <srskru@gmail.com>
2025-04-24 16:04:43 -0300

    Project created

---
+++ src/main/java/com/devsuperior/dslistgamer/DslistgamerApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslistgamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistgamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistgamerApplication.class, args);
+	}
+
+}
