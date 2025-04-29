c066419ccf11c7a2485ba7c5d94a260aff0e8a91
Felipe Lorenzon <srskru@gmail.com>
2025-04-29 15:41:52 -0300

    Exception handling

--- src/main/java/com/devsuperior/dslistgamer/services/GameService.java
+++ src/main/java/com/devsuperior/dslistgamer/services/GameService.java
@@ -1,17 +1,18 @@
 package com.devsuperior.dslistgamer.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslistgamer.dto.GameDTO;
-import com.devsuperior.dslistgamer.dto.GameListDTO;
 import com.devsuperior.dslistgamer.dto.GameMinDTO;
 import com.devsuperior.dslistgamer.entities.Game;
 import com.devsuperior.dslistgamer.projections.GameMinProjection;
 import com.devsuperior.dslistgamer.repositories.GameRepository;
+import com.devsuperior.dslistgamer.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class GameService {
@@ -21,10 +22,16 @@ public class GameService {
 		
 	@Transactional(readOnly = true)
 	public GameDTO findById(Long id) {
+		try {
 		Game result = gameRepository.findById(id).get();
 		GameDTO dto = new GameDTO(result);
 		return dto;
+		}
 		
+		catch(NoSuchElementException e){
+			throw new ResourceNotFoundException("Id não encontrado");
+		}
+
 	}
 	
 	@Transactional(readOnly = true)
